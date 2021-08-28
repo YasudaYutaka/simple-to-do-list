de8159112a65488ab92dcb6fb2ee09d5c7a7fb0e
Rafael Yutaka Yasuda <62554340+YasudaYutaka@users.noreply.github.com>
2021-08-28 15:37:50 -0300

    initial commit

---
+++ src/components/Item.js
@@ -0,0 +1,9 @@
+import styles from './Item.module.css';
+
+const Item = (props) => {
+    return (
+        <h1>asd</h1>
+    );
+}
+
+export default Item;
---
+++ src/components/ItemList.js
@@ -0,0 +1,10 @@
+import styles from './ItemList.module.css';
+import Item from './Item';
+
+const ItemList = (props) => {
+    return (
+        <Item />
+    );
+}
+
+export default ItemList;
