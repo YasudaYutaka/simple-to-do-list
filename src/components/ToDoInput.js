c26bf95cd7423fde39f33f4c62a9ca5a966b9bd9
Rafael Yutaka Yasuda <62554340+YasudaYutaka@users.noreply.github.com>
2021-08-28 15:37:17 -0300

    onChangeHandler & submitHandler

--- src/components/ToDoInput.js
+++ src/components/ToDoInput.js
@@ -1,12 +1,31 @@
+import {useState} from 'react';
 import styles from './ToDoInput.module.css';
 import Button from './Button';
 
 const ToDoInput = props => {
+
+    const [enteredValue, setEnteredValue] = useState('');
+    const [isValid, setIsValid] = useState(true);
+
+    const onChangeHandler = (event) => {
+        setEnteredValue(event.target.value);
+    }
+
+    const submitHandler = (event) => {
+        event.preventDefault();
+        setIsValid(true);
+        if(enteredValue.trim().length === 0) {
+            setIsValid(false);
+            console.log(isValid); //
+            return;
+        }
+    }
+
     return (
-        <form>
-            <div  className={styles['form-control']}>
+        <form onSubmit={submitHandler}>
+            <div className={[`${styles['form-control']} ${!isValid && styles.invalid}`]}>
                 <label>To do</label>
-                <input placeholder='type something'></input>
+                <input placeholder='type something' onChange={onChangeHandler}></input>
             </div>
             <Button type='submit'>Add</Button>
         </form>
