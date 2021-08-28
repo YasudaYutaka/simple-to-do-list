0d082bc859d059423104c778b0e757805f26a117
Rafael Yutaka Yasuda <62554340+YasudaYutaka@users.noreply.github.com>
2021-08-28 15:38:06 -0300

    items-section & ItemList component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
 import ToDoInput from './components/ToDoInput';
+import ItemList from './components/ItemList'
 
 function App() {
   return (
     <header>
       <section className='input-section'>
         <ToDoInput />
+      </section>
+      <section className='items-section'>
+        <ItemList />
       </section>
     </header>
   );
