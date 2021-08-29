29c679608a9a12e04fe0b1dde38297aa4fc1a313
Rafael Yutaka Yasuda <62554340+YasudaYutaka@users.noreply.github.com>
2021-08-29 15:21:14 -0300

    map()

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -1,10 +1,16 @@
-import styles from './ItemList.module.css';
-import Item from './Item';
+import styles from "./ItemList.module.css";
+import Item from "./Item";
 
 const ItemList = (props) => {
-    return (
-        <Item />
-    );
-}
+  return (
+    <ul className={styles.list}>
+      {props.text.map((item) => (
+        <Item key={item.id} id={item.id} onDelete={props.onDeleteItem}>
+          {item.text}
+        </Item>
+      ))}
+    </ul>
+  );
+};
 
 export default ItemList;
