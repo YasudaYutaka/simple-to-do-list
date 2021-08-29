037cb0df9c1359f238773bd026da6735833a7149
Rafael Yutaka Yasuda <62554340+YasudaYutaka@users.noreply.github.com>
2021-08-29 15:20:17 -0300

    addItemHandler & deleteItemHandler

--- src/App.js
+++ src/App.js
@@ -1,15 +1,35 @@
-import './App.css';
-import ToDoInput from './components/ToDoInput';
-import ItemList from './components/ItemList'
+import { useState } from "react";
+import "./App.css";
+import ToDoInput from "./components/ToDoInput";
+import ItemList from "./components/ItemList";
 
 function App() {
+  const [enteredText, setEnteredText] = useState([
+    { text: "Watch anime", id: "t1" },
+    { text: "Do exercises", id: "t2" }
+  ]);
+
+  const addItemHandler = (item) => {
+    setEnteredText((prevItems) => {
+      const updatedItems = [...prevItems];
+      updatedItems.unshift({ text: item, id: Math.random().toString() });
+      return updatedItems;
+    });
+  };
+
+  const deleteItemHandler = (itemID) => {
+    setEnteredText((prevItems) => {
+      const updatedItems = prevItems.filter(item => item.id !== itemID);
+      return updatedItems;
+    })
+  }
+
   return (
     <header>
-      <section className='input-section'>
-        <ToDoInput />
+      <section className="input-section">
+        <ToDoInput onAddItem={addItemHandler} />
       </section>
-      <section className='items-section'>
-        <ItemList />
+      <section className="items-section">
       </section>
     </header>
   );
